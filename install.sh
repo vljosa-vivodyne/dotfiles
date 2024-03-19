03c72104dbc3644bdd8ab9c43f827cade55d95eb
Vebjorn Ljosa <vljosa@vivodyne.com>
2024-03-19 14:16:25 -0400

    Git user config

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -eux
+
 mkdir -m 700 -p ~/.ssh
 cp ssh/authorized_keys ~/.ssh/
 chmod 600 ~/.ssh/authorized_keys
+
+git config --global user.email vljosa@vivodyne.com
+git config --global user.name "Vebjorn Ljosa"
