c20b227beb347663e0e7fb202ea0d8728ed9ef41
Vebjorn Ljosa <vljosa@vivodyne.com>
2024-03-19 14:48:42 -0400

    Pyright

--- install.sh
+++ install.sh
@@ -7,3 +7,12 @@ chmod 600 ~/.ssh/authorized_keys
 
 git config --global user.email vljosa@vivodyne.com
 git config --global user.name "Vebjorn Ljosa"
+
+# Find the first expansion of the wildcard
+PYRIGHT=$(ls -d /workspaces/hive-*/services/*/.venv/bin/pyright-langserver | head -n 1)
+if [ -z "$PYRIGHT" ]; then
+	echo "No pyright-langserver found"
+	exit 1
+fi
+sudo mkdir -p /Users/vljosa/.emacs.d/.cache/lsp/npm/pyright/bin/
+sudo ln -sf "$PYRIGHT" /Users/vljosa/.emacs.d/.cache/lsp/npm/pyright/bin/
